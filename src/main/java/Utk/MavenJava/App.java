4bf6219983a962af2ed039d57646430907e0d6b4
Utkcool <utkarsh.shakya@gmail.com>
2021-12-25 23:16:57 +0530

    first commit

---
+++ src/main/java/Utk/MavenJava/App.java
@@ -0,0 +1,7 @@
+package Utk.MavenJava;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Utk/MavenJava/AppiumTest.java
@@ -0,0 +1,20 @@
+package Utk.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void androidapp()
+	{
+		System.out.println("androidapp");
+	}
+	
+	
+	@Test
+	public void iosapp()
+	{
+	
+		System.out.println("ios");
+	}
+}
